007fd908bfd39a5324f615025ff45e452a2a0e11
Mery V <mdigbazova@gmail.com>
2019-04-26 18:17:54 +0300

    First commit testing system

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\learning\python_django_201903\examine_project\testsystem\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
